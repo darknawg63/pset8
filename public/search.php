fc3f741658328cc76b02c4d1b7b866c63c21f926
darknawg63@gmail.com <darknawg63@gmail.com>
2016-10-23 15:37:35 +0200

    pset8 search 1

--- public/search.php
+++ public/search.php
@@ -6,6 +6,12 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
+    
+    // Indeed I thought it may be more efficient to have a full-text index accross
+    // all three relevant columns.
+    $places = CS50::query("SELECT * FROM places
+                            WHERE MATCH(postal_code, place_name, admin_name1)
+                            AGAINST (?)", $_GET["geo"]);
 
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
